63531845bbc7c2d4953c055e29720dcd2a7f972e
eulbyvan <work.issdq@gmail.com>
2022-12-26 22:22:43 +0700

    toggle reminder and conditional styling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -6,9 +6,12 @@ import { FaTimes } from 'react-icons/fa';
  * Copyright (c) 2022
  */
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
 	return (
-		<div className="task">
+		<div
+			className={`task ${task.reminder ? 'reminder' : ''}`}
+			onDoubleClick={() => onToggle(task.id)}
+		>
 			<h3>
 				{task.text}{' '}
 				<FaTimes
