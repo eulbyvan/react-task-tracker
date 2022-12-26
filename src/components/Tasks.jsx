a421bc886023c20961cd132a4cebd40727a78cc2
eulbyvan <work.issdq@gmail.com>
2022-12-26 17:04:24 +0700

    global state

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,33 +1,10 @@
-import { useState } from 'react';
-
 /*
  * Author : Stu (https://www.eulbyvan.com)
  * Created on : Mon Dec 26 2022 16:25:47
  * Copyright (c) 2022
  */
 
-const Tasks = () => {
-	const [tasks, setTasks] = useState([
-		{
-			id: 1,
-			text: 'Doctors Appointment',
-			day: 'Feb 5th at 2:30pm',
-			reminder: true,
-		},
-		{
-			id: 2,
-			text: 'Meeting at School',
-			day: 'Feb 6th at 1:30pm',
-			reminder: true,
-		},
-		{
-			id: 3,
-			text: 'Food Shopping',
-			day: 'Feb 7th at 2:30pm',
-			reminder: true,
-		},
-	]);
-
+const Tasks = ({ tasks }) => {
 	return (
 		<>
 			{tasks.map((task) => (
